81fad0b402448ad9c008f14981a636517fa6c1d2
Rafael Nunes <rfnunes1000@gmail.com>
2025-12-17 13:25:56 -0300

    feat: Terminei o meu 1° banco de dados

--- Estudos/JavaScript/Back-End/API_01/server.js
+++ Estudos/JavaScript/Back-End/API_01/server.js
@@ -1,18 +1,67 @@
 import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
+app.post('/usuarios',async (req, res) => {
+     
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
-app.post('/usuarios', (req, res) => {
-     console.log(req.body)
-
-     res.send('Ok post')
+     res.send('Ok aqui deu certo')
 })
 
-app.get('/usuarios', (req, res) => {
-    res.send('Ok, deu bom')
+
+app.put('/usuarios/:id',async (req, res) => {
+     
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+     res.send('Ok aqui deu certo')
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: "Usuário deletado com sucesso"})
+})
+
+app.get('/usuarios', async (req, res) => {
+    let users = []
+
+    if(req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age,
+                email: req.query.email
+            }
+        })
+    }else{
+        users = await prisma.user.findMany()
+    }
+
+    res.status(200).json(users)
 })
 
 app.listen(3000)
@@ -24,4 +73,7 @@ app.listen(3000)
     - Listar todos os usuários
     - Editar um usuário
     - Deletar um usuário
+
+    Username: Rafael_Nunes
+    Senha: R@fa1001
 */
