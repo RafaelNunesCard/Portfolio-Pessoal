083bba8df16645874c67446fa22ac2620aba4d36
Rafael Nunes Cardoso <rfnunes1000@gmail.com>
2026-03-09 11:28:36 -0300

    Estudando Kotlin

---
+++ Estudos/Kotlin/funçõe.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println(birthdayGreeting("Rafael", 16))
+}
+
+fun birthdayGreeting(name: String, age: Int): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now $age years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
---
+++ Estudos/Kotlin/variaveis.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var cartTotal = 0
+    println("Total: $cartTotal")
+
+    cartTotal = 20
+    println("Total: $cartTotal")
+
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+
+    val notificationsEnabled: Boolean = false
+    println("Are notifications enabled? " + notificationsEnabled)
+}
